5158680354c74b25fbd45eaf709f5abad016bc1f
DixLan <93582478+DixLan@users.noreply.github.com>
2024-09-01 22:55:31 +0200

    added back to top button

--- script.js
+++ script.js
@@ -52,3 +52,22 @@ document.querySelectorAll('.gallery img').forEach(image => {
         });
     });
 });
+
+
+// Afficher le bouton lorsque l'utilisateur défile vers le bas
+window.onscroll = function() {
+    const backToTopButton = document.getElementById('back-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Remonter en haut de la page lorsque le bouton est cliqué
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
