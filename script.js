605c8000739cb9cb45640d0154bb8f9c1681b84e
DixLan <93582478+DixLan@users.noreply.github.com>
2024-09-10 19:41:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,28 +16,3 @@ document.querySelectorAll('.lightbox').forEach(image => {
         });
     });
 });
-
-// Scroll fluide pour les ancres
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-    // Observer pour déclencher l'animation d'apparition
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            }
-        });
-    });
-
-    // Applique l'animation aux éléments que l'on souhaite faire apparaître
-    document.querySelectorAll('.fade-in').forEach(el => {
-        observer.observe(el);
-    });
-    
